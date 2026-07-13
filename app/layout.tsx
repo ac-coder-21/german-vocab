718dcf7be0650a4d6120c03eff35fa9023b721bb
ac-coder-21 <auuchavan@gmail.com>
2026-07-13 21:54:46 +0200

    Revert "enabling web analytics for no reason"
    This reverts commit fc6ba8f4e9007916a880186e069821075acd4f13.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 import { ThemeToggle } from "@/components/theme-toggle";
@@ -47,7 +46,6 @@ export default async function RootLayout({
           <ThemeToggle />
         </div>
         {children}
-        <Analytics />
       </body>
     </html>
   );
