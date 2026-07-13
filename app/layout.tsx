fc6ba8f4e9007916a880186e069821075acd4f13
ac-coder-21 <auuchavan@gmail.com>
2026-07-13 21:43:49 +0200

    enabling web analytics for no reason

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 import { ThemeToggle } from "@/components/theme-toggle";
@@ -46,6 +47,7 @@ export default async function RootLayout({
           <ThemeToggle />
         </div>
         {children}
+        <Analytics />
       </body>
     </html>
   );
